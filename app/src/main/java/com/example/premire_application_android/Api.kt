986371b64cfe9802c4c5d737b3c03f097fe6a61e
Leo-Pellegrin <leo.pellegrin@etud.univ-jfc.fr>
2023-09-19 15:32:37 +0200

    Affichage des images des films + Titre bouton icon bottomnavbar

--- app/src/main/java/com/example/premire_application_android/Api.kt
+++ app/src/main/java/com/example/premire_application_android/Api.kt
@@ -1,16 +1,12 @@
 package com.example.premire_application_android
 
 import TmdbMovieResult
-import okhttp3.ResponseBody
 import retrofit2.http.GET
-import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface api {
     @GET("trending/movie/week")
     suspend fun lastmovies(@Query("api_key") api_key : String): TmdbMovieResult
 
-    @GET("{id}")
-    suspend fun image(@Path("backdrop_path") backdrop_path: String): ResponseBody
 }
 
--- app/src/main/java/com/example/premire_application_android/ListFilms.kt
+++ app/src/main/java/com/example/premire_application_android/ListFilms.kt
@@ -1,17 +1,22 @@
 package com.example.premire_application_android
 
 import FilmViewModel
-import ImageViewModel
 import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -22,23 +27,21 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.annotation.ExperimentalCoilApi
 import coil.compose.rememberImagePainter
-import java.io.File
-import java.io.FileOutputStream
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MovieScreen(){
     val filmViewModel: FilmViewModel = viewModel()
-    val imageViewModel : ImageViewModel = viewModel()
     Scaffold(
         topBar = {
             TopNavBar()
@@ -47,7 +50,7 @@ fun MovieScreen(){
             BottomNavBar()
         }
     ){
-        listFilms(filmViewModel, imageViewModel)
+        ListFilms(filmViewModel)
     }
 }
 
@@ -86,28 +89,39 @@ fun BottomNavBar(){
             Row(modifier = Modifier.fillMaxWidth(),
                 horizontalArrangement = Arrangement.SpaceEvenly){
                 IconButton(onClick = { /* do something */ }) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.movie_white_24dp),
-                        contentDescription = "Localized description",
-                        tint = Color.White
-                    )
+                    Column(verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally,) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.movie_white_24dp),
+                            contentDescription = "Localized description",
+                            tint = Color.White
+                        )
+                        Text(text = "Films", color = Color.White, fontSize = 10.sp)
+                    }
                 }
                 IconButton(onClick = { /* do something */ }) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.desktop_windows_white_24dp),
-                        contentDescription = "Localized description",
-                        tint = Color.White
-                    )
+                    Column(verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally,) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.desktop_windows_white_24dp),
+                            contentDescription = "Localized description",
+                            tint = Color.White
+                        )
+                        Text(text = "Séries", color = Color.White, fontSize = 10.sp)
+                    }
                 }
                 IconButton(onClick = { /* do something */ }) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.person_white_24dp),
-                        contentDescription = "Localized description",
-                        tint = Color.White
-                    )
+                    Column(verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally){
+                        Icon(
+                            painter = painterResource(id = R.drawable.person_white_24dp),
+                            contentDescription = "Localized description",
+                            tint = Color.White
+                        )
+                        Text(text = "Acteurs", color = Color.White, fontSize = 10.sp)
+                    }
                 }
             }
-
         },
     )
 }
@@ -115,50 +129,38 @@ fun BottomNavBar(){
 
 @OptIn(ExperimentalCoilApi::class)
 @Composable
-fun listFilms(filmVM: FilmViewModel, imageVM: ImageViewModel){
+fun ListFilms(filmVM: FilmViewModel) {
     val movies by filmVM.movies.collectAsState()
-    val image by imageVM.image.collectAsState()
 
-    if(movies.results.isEmpty()){
+    if (movies.results.isEmpty()) {
         filmVM.getFilmInitiaux()
     }
-    if(movies.results.isNotEmpty()){
-        imageVM.getImage("w185" + movies.results[0].title)
-        val inputStream = image?.byteStream()
-        val context = requireContext();
-        val file = File(context.filesDir, "${movies.results[0].title}.jpg") // Define a file path
-        val outputStream = FileOutputStream(file)
-
-        val buffer = ByteArray(4096)
-        var bytesRead: Int
-        if (inputStream != null) {
-            while (inputStream.read(buffer).also { bytesRead = it } != -1) {
-                outputStream.write(buffer, 0, bytesRead)
+    if (movies.results.isNotEmpty()) {
+        LazyVerticalGrid(columns = GridCells.Fixed(2), modifier = Modifier.padding(top = 60.dp, bottom = 60.dp)) {
+            items(movies.results) { movie ->
+                ElevatedButton(onClick = { }, modifier = Modifier.padding(20.dp)) {
+                    Column( verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier.fillMaxSize()){
+                        Image(
+                            painter = rememberImagePainter(
+                                data = "https://image.tmdb.org/t/p/w780" + movie.backdrop_path,
+                                builder = {
+                                    crossfade(true)
+                                    // Options de redimensionnement de l'image si nécessaire
+                                    size(
+                                        250,
+                                        200
+                                    ) // Par exemple, redimensionnez l'image en 200x200 pixels
+                                }),
+                            contentDescription = "Image film ${movie.title}"
+                        )
+                        Text(text = movie.title)
+                    }
+                }
             }
-            inputStream.close()
-            outputStream.close()
         }
-
-        val painter = rememberImagePainter(
-            data = image?.byteStream(),
-            builder = {
-                crossfade(true)
-            })
-
-        Image(painter = painter, contentDescription = "Image film")
     }
-
-    /*LazyVerticalGrid(columns = GridCells.Fixed(2), modifier = Modifier.padding(80.dp)){
-        items(movies.results){ movie ->
-            ElevatedButton(onClick = { }) {
-                Image(
-                    painter = painter,
-                    contentDescription = "Image film ${movie.title}"
-                )
-                Text(text = movie.title)
-            }
-        }
-    }*/
 }
 
 
